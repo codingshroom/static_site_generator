6a68a65069c44082350d7d3e474b067ef87a18b4
codingshroom <zyt3xx@gmail.com>
2025-08-15 16:23:29 +0200

    website ready for launch

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 import shutil
 from markdown_to_html_node import markdown_to_html_node
@@ -22,12 +23,12 @@ def copy_static(origin_path, target_path):
             copy_static(sub_origin_path, sub_target_path)    
 
 
-def generate_page(from_path, template_path, dest_path):
-#    print(f"generating page {from_path} to {dest_path} using {template_path}")
-    root_dir = "/Users/karl/coding/boot.dev/ssg"
-    from_path = os.path.join(root_dir, from_path)
-    template_path = os.path.join(root_dir, template_path)
-    dest_path = os.path.join(root_dir, dest_path)
+def generate_page(base_path, from_path, template_path, dest_path):
+    print(f"generating page {from_path} to {dest_path} using {template_path}")
+    root_path = "/Users/karl/coding/boot.dev/ssg"
+    from_path = os.path.join(root_path, from_path)
+    template_path = os.path.join(root_path, template_path)
+    dest_path = os.path.join(root_path, dest_path)
     with open(from_path) as f:
         markdown = f.read()
     title = extract_title(markdown)
@@ -36,6 +37,8 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
         template = template.replace("{{ Title }}", title)
         template = template.replace("{{ Content }}", html_string)
+        template = template.replace('href="/', f'href="{base_path}')
+        template = template.replace('src="/', f'src="{base_path}')
     dest_dir_name = os.path.dirname(dest_path)
     create_dirs(dest_dir_name)
     with open(dest_path, "w") as new_file:
@@ -49,35 +52,33 @@ def create_dirs(path):
     os.mkdir(path)
 
 
-def generate_pages_recursively(from_path, template_path, dest_path):
-    root_dir = "/Users/karl/coding/boot.dev/ssg"
-    from_path = os.path.join(root_dir, from_path)
-    template_path = os.path.join(root_dir, template_path)
-    dest_path = os.path.join(root_dir, dest_path)
+def generate_pages_recursively(base_path, from_path, template_path, dest_path):
+    root_path = "/Users/karl/coding/boot.dev/ssg"
+    from_path = os.path.join(root_path, from_path)
+    template_path = os.path.join(root_path, template_path)
+    dest_path = os.path.join(root_path, dest_path)
     Path(dest_path).mkdir(parents=True, exist_ok=True)
     dir_list = os.listdir(from_path)
     for entry in dir_list:
         entry_path = os.path.join(from_path, entry)
         if os.path.isfile(entry_path):
             file_path = os.path.join(dest_path, "index.html")
-            generate_page(entry_path, template_path, file_path)  
+            generate_page(base_path, entry_path, template_path, file_path)  
             continue
         dest_entry_path = os.path.join(dest_path, entry)
-        generate_pages_recursively(entry_path, template_path, dest_entry_path)
+        generate_pages_recursively(base_path, entry_path, template_path, dest_entry_path)
 
 
 
 def main():
-    copy_static("static", "public")
-    generate_pages_recursively("content", "template.html", "public")
-
-#    generate_page("index.md", "template.html", "index.html")
-#    generate_page("contact/index.md", "template.html", "contact/index.html")
-#    generate_page("blog/tom/index.md", "template.html", "blog/tom/index.html")
-#    generate_page("blog/glorfindel/index.md", "template.html", "blog/glorfindel/index.html")
-#    generate_page("blog/majesty/index.md", "template.html", "blog/majesty/index.html")
-
-
+    base_path = None
+    if len(sys.argv) >= 2:
+        base_path = sys.argv[1]
+    else:
+        base_path = "/"
+    print(f"{base_path=}")
+    copy_static("static", "docs")
+    generate_pages_recursively(base_path, "content", "template.html", "docs")
 
 
 
